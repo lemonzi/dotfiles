68c29ed004304eb5f94656552160b4cdaefadf6f
Quim Llimona <ql@lemonzi.me>
2014-11-06 10:56:11 +0100

    fix rsync

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 ## INSTALL SCRIPT
 
 # cp -r !(.git|install.sh|README.md) ~
-rsync -a . ~ --exclude=".git" --exclude="README.md" --exclude="install.sh" --exclude=".bash_config"
+
+rsync -a --exclude-from="exclude" . ~
 
