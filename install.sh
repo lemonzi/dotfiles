df7ea2ab078a483d51b9ef3453acfef578d741ef
Quim Llimona <ql@lemonzi.me>
2014-11-06 10:02:39 +0100

    linux defaults and tuning

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 ## INSTALL SCRIPT
 
 # cp -r !(.git|install.sh|README.md) ~
-rsync -a . ~ --exclude=".git" --exclude="README.md" --exclude="install.sh"
+rsync -a . ~ --exclude=".git" --exclude="README.md" --exclude="install.sh" --exclude=".bash_config"
+
