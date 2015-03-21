2a9afa78b8f038b33d1034476057147adf20821b
Quim Llimona <ql@lemonzi.me>
2015-03-21 14:07:40 +0100

    Create vim temp dirs on install

--- install.sh
+++ install.sh
@@ -6,4 +6,7 @@
 
 rsync -a --exclude-from="exclude" . ~
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+mkdir ~/.vim/backup
+mkdir ~/.vim/swap
+mkdir ~/.vim/undo
 vim +PluginInstall +qall
