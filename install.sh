8fcc6b516374cc4cd405efc44431e2c0f9638ac7
Quim Llimona <ql@lemonzi.me>
2015-07-18 11:48:17 +0200

    Added PIP automation

--- install.sh
+++ install.sh
@@ -2,11 +2,17 @@
 
 ## INSTALL SCRIPT
 
+# Dotfiles
 # cp -r !(.git|install.sh|README.md) ~
-
 rsync -a --exclude-from="exclude" . ~
+
+# Vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 mkdir ~/.vim/backup
 mkdir ~/.vim/swap
 mkdir ~/.vim/undo
 vim +PluginInstall +qall
+
+# PIP
+pip install -r pip.txt
+
