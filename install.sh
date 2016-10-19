7d7e4d7e394cf487252cb4f2489b77b74d34234a
Quim Llimona <ql@lemonzi.me>
2016-10-19 19:33:39 -0400

    install.sh now works

--- install.sh
+++ install.sh
@@ -7,17 +7,8 @@ set -eu
 
 # Dotfiles
 for f in ./dotfiles/*; do
-    ln -s $f ~/$f
+    ln -fs $(pwd)/dotfiles/$(basename "$f") ~/.$(basename "$f")
 done
-
-# Vim
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-mkdir ~/.vim/swap
-mkdir ~/.vim/undo
-vim +PluginInstall +qall
-pushd ~/.vim/bundle/YouCompleteMe
-./install.py --clang-completer --tern-completer
-popd
 
 # Homebrew
 xcode-select –install
@@ -25,11 +16,30 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew tap homebrew/bundle
 brew bundle
 
+# Vim
+mkdir ~/.vim/bundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+mkdir ~/.vim/swap
+mkdir ~/.vim/undo
+vim +PluginInstall +qall
+# YCM
+pushd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer --tern-completer
+popd
+# Color-coded
+pushd ~/.vim/bundle/color_coded
+mkdir build && cd build
+cmake ..
+make && make install # Compiling with GCC is preferred, ironically
+# Cleanup afterward; frees several hundred megabytes
+make clean && make clean_clang
+popd
+
 # PIP
-pip install -r pip.txt
+#pip install -r pip.txt
 
 # Gem
-while read gem; do
-    sudo gem install $gem
-done <gem.txt
+#while read gem; do
+#    sudo gem install $gem
+#done <gem.txt
 
