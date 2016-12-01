81e2c820b3f2efd0033e0843bc0b3a2fd66d42c1
Quim Llimona <ql@lemonzi.me>
2016-12-01 18:24:36 -0500

    Fix pip.txt path in install.sh

--- install.sh
+++ install.sh
@@ -20,16 +20,16 @@ mkdir -p ~/.vim/undo
 vim +PluginInstall +qall
 # YCM
 pushd ~/.vim/bundle/YouCompleteMe
-# With JavaScript: add --tern-completer
-./install.py --clang-completer
+    # With JavaScript: add --tern-completer
+    ./install.py --clang-completer
 popd
 # Color-coded
 pushd ~/.vim/bundle/color_coded
-mkdir -p build && cd build
-cmake ..
-make && make install # Compiling with GCC is preferred, ironically
-# Cleanup afterward; frees several hundred megabytes
-make clean && make clean_clang
+    mkdir -p build && cd build
+    cmake ..
+    make && make install # Compiling with GCC is preferred, ironically
+    # Cleanup afterward; frees several hundred megabytes
+    make clean && make clean_clang
 popd
 
 # PIP
