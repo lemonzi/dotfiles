81d1fb654ff35124c740e6995c450977b9f8f990
Quim Llimona <ql@lemonzi.me>
2017-02-02 12:28:53 -0500

    Bugfixes and install.sh arguments

--- install.sh
+++ install.sh
@@ -5,11 +5,34 @@ set -eu
 # INSTALL SCRIPT
 # Requires: vim, clang, tern (optional), pip / python, gem / ruby
 
+# Parse arguments
+_COMPILE=true
+_GEM=true
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --no-recompile)
+            _COMPILE=false
+            ;;
+        --no-gems)
+            _GEM=false
+            ;;
+        -h|--help)
+            echo 'Usage: ./install.sh [--no-recompile] [--no-gems]'
+            exit 0
+            ;;
+    esac
+    shift # get next argument
+done
+
 # Dotfiles
 for f in ./dotfiles/*; do
-    if [ -L ~/.$(basename "$f") ]; then
+    if [ -d ~/.$(basename "$f")/ ]; then
         # Prevent name collisions, which result in unwanted nested folders
-        rm -ri ~/.$(basename "$f")
+        if [ -L ~/.$(basename "$f") ]; then
+            rm ~/.$(basename "$f")
+        else
+            mv ~/.$(basename "$f") ~/.$(basename "$f").old
+        fi
     fi
     ln -fs $(pwd)/dotfiles/$(basename "$f") ~/.$(basename "$f")
 done
@@ -25,16 +48,20 @@ mkdir -p ~/.vim/undo
 vim +PluginInstall +qall
 { # Temporal fix because gitsessions.vim is broken
     pushd ~/.vim/bundle/gitsessions.vim
-    git fetch origin pull/12/head:fix_syntax
-    git checkout fix_syntax
+    git checkout -B fix_syntax
+    git pull origin pull/12/head
     popd
 }
-compile_youcompleteme
-compile_colorcoded
+if $_COMPILE; then
+    compile_youcompleteme
+    compile_colorcoded
+fi
 
 # Tmux plug-ins
-mkdir -p ~/.tmux/plugins
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+    mkdir -p ~/.tmux/plugins
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
 ~/.tmux/plugins/tpm/bin/install_plugins
 
 # PIP
@@ -45,10 +72,12 @@ else
 fi
 
 # Gem
-if command_exists gem; then
-    while read gem_name; do
-        sudo gem install $gem_name
-    done <./gem.txt
-else
-    echo "Unable to install Ruby gems; gem not found." 1>&2
+if $_GEM; then
+    if command_exists gem; then
+        while read gem_name; do
+            sudo gem install $gem_name
+        done <./gem.txt
+    else
+        echo "Unable to install Ruby gems; gem not found." 1>&2
+    fi
 fi
